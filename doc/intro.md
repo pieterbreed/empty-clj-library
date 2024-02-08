d1d8fef7dcad05ec5da2505fdd326142d226436b
Pieter Breed <pieter@pb.co.za>
2024-02-08 12:07:43 +0000

    committing empty lein lib template files

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to empty-clj-library
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/empty_clj_library/core_test.clj
@@ -0,0 +1,7 @@
+(ns empty-clj-library.core-test
+  (:require [clojure.test :refer :all]
+            [empty-clj-library.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
